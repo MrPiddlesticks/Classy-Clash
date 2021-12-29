a2825761c8796fe921689e4e362ceb77fb3e6c5f
MrPiddlesticks <44594756+MrPiddlesticks@users.noreply.github.com>
2021-12-28 22:26:10 -0500

    Update Prop.h

--- Prop.h
+++ Prop.h
@@ -4,6 +4,8 @@ class Prop
 {
 public:
     Prop(Vector2 pos, Texture2D tex);
+    void Render(Vector2 KnightPos);
+    Rectangle getCollisionRec(Vector2 KnightPos);
 private:
     Texture2D texture{};
     Vector2 worldPos{};
